5f3a3dd7c8e9c866b064b7349ac74fb56d3af42d
SmallJoker <mk939@ymail.com>
2016-10-07 13:36:10 +0200

    Define some stupid M_PI's

--- src/collision.cpp
+++ src/collision.cpp
@@ -17,6 +17,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
+#define M_PI 3.1415926536
 #include "collision.h"
 #include "mapblock.h"
 #include "map.h"
